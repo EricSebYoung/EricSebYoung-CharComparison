aa698775dbf78d45e1eafae7acc2d329f8cc850f
EricSebYoung <ericsyoung@live.com>
2023-02-24 14:42:38 +0000

    Char Comparison Completed

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -11,6 +11,23 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
+
+        // Compare array sizes.
+        if (a.length > b.length) {
+            return 1;
+        } else if (a.length < b.length) {
+            return -1;
+        } else {
+            // Compare individual characters
+            for (int i=0; i < a.length; i++) {
+                if (a[i] > b[i]) {
+                    return 1;
+                } else if (a[i] < b[i]) {
+                    return -1;
+                }
+            }
+        }
+        // Reaches here if both are equal
         return 0;
     }
 }
